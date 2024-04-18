877042ceb5b7c55dc612eea0d257237967407591
Pratyush Singh <61535384+Pratyush1009@users.noreply.github.com>
2024-04-18 10:23:25 +0530

    Utils Code

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+//By defining extended property we can pass object inside object, i.e., we can pass nested objects
+
+app.use(express.static("public"))
+//Use static to store the files and folders like pdf images in the local public asset
+
+app.use(cookieParser())
+
+export default app
--- src/index.js
+++ src/index.js
@@ -3,31 +3,21 @@ import dotenv from "dotenv"
     instead of require for dotenv we can use import dotenv. Ref - Line 8 package.json */
 
 import connectDB from "./db/connectDB.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
+
 connectDB()
-
-
-// import express from "express"
-
-// const app = express()
-
-// //Effy
-// ;( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error", ()=>{console.log("Error :",error);
-//         throw error
-//     })
-//     app.listen(process.env.PORT, () => {
-//         console.log(`Application is running on port ${process.env.PORT}`);
-//     })
-
-//     } catch (error) {
-//         console.log("Error while connecting to DB ",error);
-//         throw error
-//     }
-// })()
+.then(()=>{
+    app.on("error", (error) => {
+        console.log("ERROR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Application is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{console.log("Mongo DB connection failed !!! ",err);})
