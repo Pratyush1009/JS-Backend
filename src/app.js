7e5dfb01a81b05e031df5d217ce09b9514cbe1a7
Pratyush Singh <61535384+Pratyush1009@users.noreply.github.com>
2024-05-09 11:48:30 +0530

    Added comments in app.js file

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.use(cors({
     credentials: true
 }))
 
+//We prefer app.use() when we need to configure some settings or middleware
+
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 //By defining extended property we can pass object inside object, i.e., we can pass nested objects
